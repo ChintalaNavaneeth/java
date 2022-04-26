65986839782bc6cb48dfb381a9f43292433d15ed
navaneeth chintala <navaneethchintala1304@gmail.com>
2022-04-27 00:37:57 +0530

    java_two

---
+++ src/arrays/two.java
@@ -0,0 +1,19 @@
+package arrays;
+
+import java.util.Arrays;
+
+//https://leetcode.com/problems/concatenation-of-array/
+public class two {
+    public static void main(String[] args) {
+        int [] nums = {1,2,1};
+        System.out.println(Arrays.toString(getConcatenation(nums)));
+    }
+    public static int[] getConcatenation(int[] nums) {
+        int ans[] = new int[nums.length * 2];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[i];
+            ans[i+nums.length] = nums[i];
+        }
+        return ans;
+    }
+}
