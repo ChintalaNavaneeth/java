40e6a960008b6a29cbf97c765d167d1955782fa3
navaneeth chintala <navaneethchintala1304@gmail.com>
2022-05-07 11:22:58 +0530

    thirteenandfourteen

---
+++ src/arrays/fourteen.java
@@ -0,0 +1,26 @@
+package arrays;
+
+//https://leetcode.com/problems/cells-with-odd-values-in-a-matrix/
+
+import java.util.Arrays;
+
+public class fourteen {
+    public static void main(String[] args) {
+        int m = 2;
+        int n = 3;
+        int index[][] = {{0,1},{1,1}};
+
+        oddCells(m,n,index);
+    }
+    public static int oddCells(int m, int n, int[][] indices) {
+        int ans[][] = new int[m][n];
+        Arrays.fill(ans,0);
+        for (int x[]:indices) {
+            for(int y : x)
+            {
+
+            }
+        }
+
+    }
+}
